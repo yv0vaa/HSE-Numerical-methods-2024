6e012ab105a34ca4f359e40ef097e65d57d8620f
yvovaa <zakharov.2004@yandex.ru>
2024-01-28 15:05:48 +0300

    some garbage removed

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "exp.hpp"
-
-int main() {
-	float f = 3.0;
-	double d = 10.0;
-	long double ld = 0.001;
-	std::cout << ADAAI::Exp(f) << ' ' << ADAAI::Exp(d) << ' ' << ADAAI::Exp(ld) << '\n';  
-}
