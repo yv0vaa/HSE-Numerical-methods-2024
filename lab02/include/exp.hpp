1607e78ceebee394ff2898a42184cdcda505c515
yvovaa <zakharov.2004@yandex.ru>
2024-02-11 22:45:38 +0300

    demo

--- lab02/include/exp.hpp
+++ lab02/include/exp.hpp
@@ -55,33 +55,20 @@ constexpr F Exp(F x) {
         return std::ldexp(f1, static_cast<int>(n));
     } else {
         F denominator = 0.0, numerator =  0.0;
-        if constexpr (std::is_same_v<float, F>) {
-            F a[] = {1.0, 0.5, 3.0 / 28.0, 1.0 / 84.0, 1.0 / 1680.0};
-            for (int i = 4; i >= 0; i--) {
-                numerator += a[i];
-                numerator *= x1;
-                denominator += (i % 2 == 0 ? a[i] : -a[i]);
-                denominator *= x1;
-
-            }
-        } else if constexpr (std::is_same_v<double, F>) {
-            F a[] = {1.0, 0.5, 3.0 / 26.0, 5.0 / 312.0, 5.0 / 3432.0, 1.0 / 11440., 1.0 / 308880.0, 1.0 / 17297280.0};
-            for (int i = 7; i >= 0; i--) {
-                numerator += a[i];
-                numerator *= x1;
-                denominator += (i % 2 == 0 ? a[i] : -a[i]);
-                denominator *= x1;
-
-            }
-        } else {
-            F a[] = {1.0, 0.5, 7.0 / 60.0, 1.0 / 60.0, 1.0 / 624.0, 1.0 / 9360.0, 1.0 / 205920.0, 1.0 / 7207200.0, 1.0 / 518918400.0};
-            for (int i = 8; i >= 0; i--) {
-                numerator += a[i];
-                numerator *= x1;
-                denominator += (i % 2 == 0 ? a[i] : -a[i]);
-                denominator *= x1;
-
-            }
+        F a[9] = {1.0, 0.5, 3.0 / 28.0, 1.0 / 84.0, 1.0 / 1680.0};
+        int n = 4;
+        if constexpr (std::is_same_v<double, F>) {
+            a[2] = 3.0 / 26.0, a[3] = 5.0 / 312.0, a[4] = 5.0 / 3432.0, a[5] = 1.0 / 11440.0, a[6] = 1.0 / 308880.0, a[7] = 1.0 / 17297280.0;
+            n = 7;
+        } else if (std::is_same_v<long double, F>) {
+            a[2] = 7.0 / 60.0, a[3] = 1.0 / 60.0, a[4] = 1.0 / 624.0, a[5] = 1.0 / 9360.0, a[6] = 1.0 / 205920.0, a[7] = 1.0 / 7207200.0, a[8] = 1.0 / 518918400.0;
+            n = 8;
+        }
+        for (int i = n; i >= 1; i--) {
+            numerator *= x1;
+            numerator += a[i];
+            denominator *= x1;
+            denominator += (i % 2 == 0 ? a[i] : -a[i]);
         }
         return std::ldexp(numerator / denominator, static_cast<int>(n));
     }
