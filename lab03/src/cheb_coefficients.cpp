4935005b3ea0c762274ba0adbcfd30443c90c97b
Fedchin Sergey <serg_fedchn@Sergey_Laptop.localdomain>
2024-02-19 13:54:15 +0300

    Implement SLE solution

--- lab03/src/cheb_coefficients.cpp
+++ lab03/src/cheb_coefficients.cpp
@@ -1,7 +1,10 @@
 #include <vector>
 #include <iostream>
+#include <gsl/gsl_linalg.h>
 
-long long a_coeficient(long long n, long long k) {
+enum {N = 2};
+
+long long a_nk(long long n, long long k) {
 	if (n % 2 == 0) {
 		if (k % 2 == 0) return 0;
 		if (k == 1) return n;
@@ -20,30 +23,79 @@ long long T_0(long long n) {
 	return -1;
 }
 
-std::vector<std::vector<long long>> cheb_coefficients(long long N) {
+std::vector<std::vector<long long>> SLE_coefficients() {
 	std::vector<std::vector<long long>> result(N + 1, std::vector<long long>(N + 1));
-	for (int k = 0; k < N; k++) {
+	for (int k = 0; k <= N - 1; k++) {
 		result[k][k] = -1;
-		for (int n = k + 1; n < N + 1; n++) {
-			result[k][n] = a_coeficient(n, k);
+		for (int n = k + 1; n <= N; n++) {
+			result[k][n] = a_nk(n, k);
 		}
 	}
-	for (int n = 0; n < N+1; n++) {
+	for (int n = 0; n <= N; n++) {
 		result[N][n] = T_0(n);
 	}
 	return result;
 }
 
+// converts vector<long long> to double[]
+double *v_to_array(std::vector<long long> &vec) {
+	static double data[(N + 1) * (N + 1)];
+	for (int j = 0; j < N + 1; j++) {
+		data[j] = static_cast<double>(vec[j]);
+	}
+	return data;
+}
+
+// converts vector<vector<long long>> to double[]
+double *vv_to_array(std::vector<std::vector<long long>> &vecvec) {
+	static double data[(N + 1) * (N + 1)];
+	for (int i = 0; i < N + 1; i++) {
+		for (int j = 0; j < N + 1; j++) {
+			data[i * (N + 1) + j] = static_cast<double>(vecvec[i][j]);
+		}
+	}
+	return data;
+}
+
 int main() {
-	int N = 3;
-	std::vector<std::vector<long long>> test = cheb_coefficients(N);
-	for (int i = 0; i < test.size(); i++) {
-		for (int j = 0; j < test[i].size(); j++) {
-			std::cout << test[i][j] << " ";
+	std::vector<std::vector<long long>> coeffs = SLE_coefficients();
+	for (int i = 0; i < coeffs.size(); i++) {
+		for (int j = 0; j < coeffs[i].size(); j++) {
+			std::cout << coeffs[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
 	std::vector<long long> RHS(N + 1);
 	RHS[N] = 1;
-	//now we need to solve linear system with[ cheb coefficients = RHS ]
+
+	//now we need to solve linear system with[ SLE_coefficients * C = RHS ]
+
+	// Convert 
+	double *matrix = vv_to_array(coeffs);
+	gsl_matrix_view A = gsl_matrix_view_array(matrix, N + 1, N + 1);
+	std::cout << "A = \n";
+	gsl_matrix_fprintf(stdout, &A.matrix, "%lf");
+
+	double *vec = v_to_array(RHS);
+	gsl_vector_view b = gsl_vector_view_array(vec, N + 1);
+	std::cout << "b = \n";
+	gsl_vector_fprintf(stdout, &b.vector, "%lf");
+
+	gsl_vector *x = gsl_vector_alloc(N + 1);
+	gsl_permutation *perm = gsl_permutation_alloc(N + 1);
+
+	// Decompose A into the LU form:
+	int signum;
+	gsl_linalg_LU_decomp(&A.matrix, perm, &signum);
+
+	// Solve the linear system
+	gsl_linalg_LU_solve(&A.matrix, perm, &b.vector, x);
+
+	// Print the solution
+	std::cout << "\nx = \n";
+	gsl_vector_fprintf(stdout, x, "%lf");
+
+	// Release the memory previously allocated for x and perm
+	gsl_vector_free(x);
+	gsl_permutation_free(perm);
 }
