0bcc4eb488abe8953bafb1dc5d2e2033fa37d020
Fedchin Sergey <serg_fedchn@Sergey_Laptop.localdomain>
2024-02-19 09:39:19 +0300

    Fix recursive fomula mistake and add polynom printing

--- lab03/include/cheb_poly.hpp
+++ lab03/include/cheb_poly.hpp
@@ -2,5 +2,10 @@
 #define CHEB_POLY_HPP
 
 #include <vector>
+#include "../src/cheb_poly.cpp"
+
 std::vector<std::vector<long long>> cheb_poly(int n);
+
+void print_cheb_poly(const std::vector<long long> &coeff);
+
 #endif
--- lab03/main.cpp
+++ lab03/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <gsl/gsl_sf_bessel.h>
 #include "include/cheb_poly.hpp"
+
 int main () {
-	auto res = cheb_poly(30);
+	auto res = cheb_poly(10);
 	for (auto elem : res) {
 		for (int a : elem) {
 			std::cout << a << ' ';
 		}
 		std::cout << '\n';
+		print_cheb_poly(elem);
+		std::cout << "\n";
 	}
 	/*double x = 5.0;
 	double y = gsl_sf_bessel_J0 (x);
