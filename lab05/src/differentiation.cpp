bcbf9150a31652d146664d88b9928a3b78d509f8
ArtemCh101 <112023266+ArtemCh101@users.noreply.github.com>
2024-03-04 13:31:46 +0300

    Create differentiation.cpp

---
+++ lab05/src/differentiation.cpp
@@ -0,0 +1,14 @@
+#include "differentiation.hpp"
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+//for now some random stuff, Not sure if this file is needed at all :)
+double func(double x, double y) {
+    return (x*x + y);
+}
+	int main() {
+    std::cout
+        << Differentiator<WhichD::XY, DiffMethod::Stencil5>(
+               &func, 4.0, 6.0) << std::endl;
+}
