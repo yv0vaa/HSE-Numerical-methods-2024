4d143fc373844c9f8e655c2d299ccab9dd2028bf
ArtemCh101 <112023266+ArtemCh101@users.noreply.github.com>
2024-03-18 19:01:39 +0300

    Update test.cpp

--- lab06/src/test.cpp
+++ lab06/src/test.cpp
@@ -1,8 +1,10 @@
 // #include "../include/Pressure.hpp"
 #include "../include/CD.hpp"
+#include "../include/u_derivative.hpp"
 #include <iostream>
 
 int main() {
-    CD CD_instance;
-    std::cout << 0.93 << std::endl;
+    double u[4] = {10000, 1160, 100000, 1160};
+    double *p = u;
+    double *derivative = u_derivative(p);
 }
