0afc30addf6d0b5873b41e47c23ad8c1a6eec8ab
Sergey Fedchin <sergfedchin@gmail.com>
2024-03-20 21:32:35 +0300

    Make CD a continuous function

--- lab06/src/CD.cpp
+++ lab06/src/CD.cpp
@@ -4,7 +4,7 @@
 
 double CD::operator()(double M) const {
     assert(M >= 0.4);
-    if (M <= 2.2) {
+    if (M <= 2) {
         for (int i = 0; i + 1 < this->arr_size; i++) {
             double M_i = 0.4 + i * 0.05;
             double M_i_1 = 0.4 + (i + 1) * 0.05;
@@ -15,7 +15,7 @@ double CD::operator()(double M) const {
             }
         }
     }
-    if (M > 2.2) {
+    if (M > 2) {
         return this->a * pow(M, -b);
     }
     return -1.0; // should not be reached
