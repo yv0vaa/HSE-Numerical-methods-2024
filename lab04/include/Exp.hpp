ea2bb972b7267123372ccff04faa40ed746f3f88
ArtemCh101 <112023266+ArtemCh101@users.noreply.github.com>
2024-06-11 17:46:16 +0300

    Update Exp.hpp

--- lab04/include/Exp.hpp
+++ lab04/include/Exp.hpp
@@ -16,7 +16,7 @@ F Exp(F x, int N, bool use_numerical_a_k) {
 }
 
 template <typename F> 
-F Exp_FFT(F gx,F x, int N, bool use_numerical_a_k) {
+F Exp_FFT(F x, int N, bool use_numerical_a_k) {
     F sum = use_numerical_a_k ? numerical_a_k(0, N) / 2 : analytical_a_k(0) / 2;
     double data[N];
     if (use_numerical_a_k) {
@@ -34,27 +34,14 @@ F Exp_FFT(F gx,F x, int N, bool use_numerical_a_k) {
     work = gsl_fft_real_workspace_alloc(N);
     real = gsl_fft_real_wavetable_alloc(N);
     gsl_fft_real_transform(data, 1, N, real, work);
-    gsl_fft_real_wavetable_free(real);
-    for (int i = 0; i < N; i++) {
-        data[i] += sum;
-    }
-    F minimal_difference = 1000.0;
-    int ind_of_minimal_difference = 0;
-
-    for (int i = 0; i < N; i++) {
-        if (abs(data[i] - gx) < minimal_difference) {
-            ind_of_minimal_difference = i;
-            minimal_difference = abs(data[i] - gx);
-        }
-    }
-    int ind = floor(x * (N + 1) / M_PI / 2);
-    std::cout << "----------------" << std::endl;
-    std::cout << minimal_difference << std::endl;
-    std::cout << abs(data[ind]-x) << std::endl;
-    std::cout << ind_of_minimal_difference << std::endl;
-    std::cout << ind << std::endl;
     
-    return data[ind_of_minimal_difference];
+    for (int i = 0; i < N; i++) {
+        sum += data[i];
+    }
+
+    gsl_fft_real_wavetable_free(real);
+    gsl_fft_real_workspace_free(work);
+    return sum;
 }
 
 // Recommended N is at least 500!
